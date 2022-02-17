399bcf71cfa264c1d9d827b2e32fb5b727e2cda4
romanpozdnyakov <romanpozdnuakov@list.ru>
2022-02-17 15:01:47 +0300

    binary-search

---
+++ labs/binary-search/test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+IFS=$'\n'
+
+name=binary-search
+files=main.cpp
+cc=g++
+
+$cc $files -o $name
+
+tests=$(find ./../../$name/ -name *.in)
+tests=$(echo $tests | tr " " "\n" | sort)
+
+for test in $tests
+do
+    test=${test%.in}
+    test_name=${test##*/}
+    ./$name < "${test}.in" > "${name}.out"
+    error=$(diff "${test}.out" "${name}.out")
+    if [[ -n $error ]]; then
+        echo "test ${test_name} failed"
+        echo "must be:"
+        cat "${test}.out"
+        echo "got:"
+        cat "${name}.out"
+        break
+    else
+        echo "test ${test_name} passed"
+    fi
+done
+
+rm "${name}.out"
+rm $name
