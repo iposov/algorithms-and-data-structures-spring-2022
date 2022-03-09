642f198db472619a7ded9f1721b58ab0784fee33
Grigory <grial00.06@gmail.com>
2022-03-09 17:35:25 +0300

    Двоичное дерево поиска

---
+++ binary-search-tree/program/program.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <set>
+
+int main()
+{
+	std::set<int> tree;
+	int N, x;
+
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> x;
+		if (!tree.count(x))
+		{
+			std::cout << "-"<<std::endl;
+			tree.insert(x);
+		}
+		else
+		{
+			std::cout << "+" << std::endl;
+		}
+	}
+
+	return 0;
+}
---
+++ binary-search-tree/program2/program2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <set>
+
+int main()
+{
+	std::set<int> tree;
+	int N, x, j;
+
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> x;
+		if (!tree.count(x))
+		{
+			std::cout << "-";
+			tree.insert(x);
+		}
+		else
+		{
+			std::cout << "+";
+		}
+
+		auto it = tree.find(x); 
+		++it;
+		if (it != tree.end())
+		{
+			std::cout << " " << *it << std::endl;
+		}
+		else
+		{
+			std::cout << " -" << std::endl;
+		}
+	}
+
+	return 0;
+}
