3c82619d4215c8cce4e3a0bba4f97a105eb799f4
Sibiryakov_Ivan <ivan.sibiryakov27@gmail.com>
2022-03-12 13:13:02 +0300

    Added decision of first task

---
+++ binary-search/FirstTask.java
@@ -0,0 +1,51 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FirstTask {
+
+  public static void main(String[] args) throws IOException {
+
+    try (
+        FileInputStream fis = new FileInputStream("./binary-search/4.in");
+        PrintStream ps = new PrintStream(new FileOutputStream("./binary-search/program.out"))
+    ) {
+      System.setIn(fis);
+      Scanner sc = new Scanner(System.in);
+      int amountOfArrayElements = Integer.parseInt(sc.nextLine());
+      String line = sc.nextLine();
+      int[] arr = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+      int amountOfNumsToSearch = Integer.parseInt(sc.nextLine());
+
+      System.setOut(ps);
+      for (int i = 0; i < amountOfNumsToSearch; i++) {
+        int numToSearch = Integer.parseInt(sc.nextLine());
+        System.out.println(binarySearch(arr, 0, amountOfArrayElements - 1, numToSearch));
+      }
+    }
+  }
+
+  public static int binarySearch(int[] arr, int left, int right, int numToSearch) {
+    if (right >= left) {
+      int middle = left + (right - left) / 2;
+
+      if (arr[middle] == numToSearch) {
+        return middle;
+      }
+
+      if (arr[middle] > numToSearch) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+
+      return binarySearch(arr, left, right, numToSearch);
+    }
+
+    return -1;
+  }
+}
