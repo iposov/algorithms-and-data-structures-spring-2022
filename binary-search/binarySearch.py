d078f2030d9b9e5621a25e822e49898483f025e1
NVRM0R <vovavelho@gmail.com>
2022-03-13 12:03:01 -0400

    add comments

--- binary-search/binarySearch.py
+++ binary-search/binarySearch.py
@@ -1,10 +1,12 @@
 #!/bin/python
 
+# читаем из файла массив с числами
 ArrayLength = input()
 ArrayStr = input()
 ArrayMapped = map(int,ArrayStr.split(' '))
 Array = list(ArrayMapped)
 
+# метод принимает рабочий массив и искомое число
 def binSearch(Array,searchNum):
     left = 0
     right = len(Array)-1
@@ -18,7 +20,9 @@ def binSearch(Array,searchNum):
             right = middle-1
     return -1
 
+# читаем количество чисел, которые надо найти
 searchNumCount = int(input())
+# searchNumCount читаем число и ищем его в массиве
 for x in range(searchNumCount):
     num = int(input())
     print(binSearch(Array,num))
