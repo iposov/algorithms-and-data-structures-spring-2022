4a424ea6c868cf0c78b91303626184c259e23902
Kirill Mokhnatkin <librarian4832@gmail.com>
2022-03-16 18:41:57 +0300

    Finished binary search algorithm

---
+++ binary-search/BinarySearch.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        try {
+            for (int fileId = 1; fileId <= 5; fileId++) {
+                //to read
+                File file = new File("./binary-search/" + fileId + ".in");
+                Scanner myReader = new Scanner(file);
+                //to write
+                FileWriter writer = new FileWriter("./binary-search/" + fileId + "_mine.out", false);
+
+                int n = myReader.nextInt();
+                Integer[] initArr = new Integer[n];
+                for (int i = 0; i < n; i++) {
+                    initArr[i] = myReader.nextInt();
+                }
+                int k = myReader.nextInt();
+                for (int i = 0; i < k; i++) {
+                    //reading next value
+                    int value = myReader.nextInt();
+                    int left = 0;
+                    int right = n - 1;
+                    int middle;
+                    do {
+                        middle = (right + left) / 2;
+                        if (value < initArr[middle])
+                            right = middle;
+                        else
+                            left = middle;
+                    } while (value != initArr[middle] && (left + 1) != right && left != right);
+                    if (value == initArr[middle])
+                        writer.write(middle + "\n");
+                    else if (value == initArr[right])
+                        writer.write(right + "\n");
+                    else if (value == initArr[left])
+                        writer.write(left + "\n");
+                    else
+                        writer.write(-1 + "\n");
+                }
+                //closing input
+                myReader.close();
+                //closing output
+                writer.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
