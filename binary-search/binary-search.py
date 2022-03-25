1c256c32899b93d4b09591e534141bec1a1ee417
VladisSer <vladisser2ex4@gmail.com>
2022-03-25 15:31:57 +0300

    Do binary search

---
+++ binary-search/binary-search.py
@@ -0,0 +1,27 @@
+import math
+
+
+def binary_search(x_i, A):
+    left = 0
+    right = len(A) - 1
+
+    while left <= right:
+        middle = math.floor((left + right)/2)
+        if A[middle] < x_i:
+            left = middle + 1
+        elif A[middle] > x_i:
+            right = middle - 1
+        else:
+            return middle
+    return -1
+
+
+N = int(input())
+A = list(map(int, input().split()))
+K = int(input())
+for i in range(K):
+    x_i = int(input())
+    print(binary_search(x_i, A))
+    print(binary_search(x_i, A))
+
+
