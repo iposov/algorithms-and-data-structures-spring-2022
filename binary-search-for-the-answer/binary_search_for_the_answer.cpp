e3cde2a78abc708642544dda0c837e60b3d92396
Tkachev-Igor <Igor9605514546@yandex.ru>
2022-03-29 14:21:59 +0300

    binary-search-for-the-answer

---
+++ binary-search-for-the-answer/binary_search_for_the_answer.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int binSearchAnswer(long* Arr, int N, int K) {;
+    int result, left, right, middle;
+    left = 0;
+    right = ((N > K) ? int((Arr[N-1] - Arr[0])/K) : 0);
+    while (true) {
+        if (right - left <= 1) {
+            result = right;
+            break;
+        }
+        else {
+            middle = (left + right)/2;
+            int j = 0;
+            for (int i = 0; (i < K) && (j < N); i++) {
+                int passLeft = Arr[j];
+                while ((Arr[j] <= passLeft + middle) && (j < N)) {
+                    j++;
+                }
+            }
+            if (j == N) {
+                right = middle;
+                }
+            else {
+                left = middle;
+            }
+        }
+
+    }
+    return result;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    int K;
+    cin >> K;
+
+    long Arr[N];
+    for (int i = 0; i < N; i++) {
+        cin >> Arr[i];
+    }
+
+    cout << binSearchAnswer(Arr, N, K) << endl;
+
+    return 0;
+}
