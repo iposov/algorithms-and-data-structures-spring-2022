5032c142bfbce13c34ebb5d099e16a2a04070728
Zemtsov Renat <zemtsov20@mail.ru>
2022-03-30 20:35:57 +0300

    union find forest solved with heuristics

---
+++ solutions/src/com/solution/UnionFindForest.java
@@ -0,0 +1,65 @@
+package com.solution;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class UnionFindForest {
+    private final int[] unionFindForestArray;
+    private final int[] ranks;
+
+    UnionFindForest(int n) {
+        this.unionFindForestArray = IntStream.range(0, n).toArray();
+        this.ranks = new int[n];
+    }
+
+    public static void main(String[] args) {
+        Iterator<String> iterator = new Scanner(System.in).tokens().iterator();
+
+        int n = Integer.parseInt(iterator.next()),
+            k = Integer.parseInt(iterator.next()),
+            first,
+            second;
+
+        UnionFindForest unionFindForest = new UnionFindForest(n);
+
+        for (int i = 0; i < k; i++) {
+            first = Integer.parseInt(iterator.next());
+            second = Integer.parseInt(iterator.next());
+            System.out.println(unionFindForest.union(first, second));
+        }
+    }
+
+    public String union(int first, int second) {
+        int firstRoot = getRoot(first),
+            secondRoot = getRoot(second);
+
+        if (firstRoot == secondRoot)
+            return "YES";
+
+        if (ranks[firstRoot] > ranks[secondRoot]) {
+            ranks[firstRoot]++;
+            unionFindForestArray[secondRoot] = firstRoot;
+        }
+        else {
+            ranks[ secondRoot]++;
+            unionFindForestArray[firstRoot] = secondRoot;
+        }
+
+        return "NO";
+    }
+
+    private int getRoot(int idx) {
+        int temp = unionFindForestArray[idx];
+        List<Integer> idsToChange = new ArrayList<>();
+        while (unionFindForestArray[temp] != temp) {
+            temp = unionFindForestArray[temp];
+            idsToChange.add(temp);
+        }
+
+        int finalTemp = temp;
+        idsToChange.forEach(i -> unionFindForestArray[i] = finalTemp);
+
+        return finalTemp;
+    }
+
+}
