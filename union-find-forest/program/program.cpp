2d76dda0d42384eeed9b27e7c1400d37c0b7ede6
Grigory <grial00.06@gmail.com>
2022-03-30 21:40:34 +0300

    add union-find-forest

---
+++ union-find-forest/program/program.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+int* arr = nullptr;
+int* rang = nullptr;
+
+int Find(int x) 
+{
+    if (x == arr[x])
+    {
+        return x;
+    }
+
+    return (arr[x] = Find(arr[x]));
+}
+
+bool Union (int x, int y)
+{
+    int X, Y;
+    if ((X = Find(x)) == (Y = Find(y)))
+    {
+        return true;
+    }
+
+    if (rang[X] < rang[Y]) 
+    {
+        arr[X] = Y;
+    }
+    else 
+    {
+        arr[Y] = X;
+        if (rang[X] == rang[Y]) 
+        {
+            ++rang[X];
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    int N, K, a, b;
+    std::cin >> N >> K;
+
+    arr = new int[N];
+    rang = new int[N];
+    for (int i = 0; i < N; ++i) 
+    {
+        rang[i] = 1;
+        arr[i] = i;
+    }
+
+    for (int i = 0; i < K; ++i) 
+    {
+        std::cin >> a >> b;
+        std::cout << (Union(a, b) ? "YES" : "NO") << std::endl;
+    }
+}
