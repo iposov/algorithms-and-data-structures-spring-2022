c553d13ff959fe928fe236db4d4652e668b3e620
Tkachev-Igor <Igor9605514546@yandex.ru>
2022-04-08 15:12:16 +0300

    union-find-forest

---
+++ union-find-forest/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *head;
+    Node *next;
+    Node *tail;
+    Node() {}
+    Node(int a) {
+        data = a;
+        head = this;
+        next = nullptr;
+        tail = this;
+    }
+};
+
+bool join(Node*& x, Node* y) {
+    if (x->head == y->head) {
+        return false;
+    }
+    x->tail->next = y;
+    x->tail = y->tail;
+    while (y != nullptr) {
+        y->head = x->head;
+        y = y->next;
+    }
+    return true;
+}
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    Node* DSU[N];
+    for (int i = 0; i < N; i++) {
+        DSU[i] = new Node(i);
+    }
+
+    for (int i = 0; i < K; i++) {
+        int m1, m2;
+        cin >> m1 >> m2;
+
+        if (join(DSU[m1],DSU[m2])) {
+            cout << "NO" << endl;
+        } else {
+            cout << "YES" << endl;
+        }
+    }
+
+    return 0;
+}
