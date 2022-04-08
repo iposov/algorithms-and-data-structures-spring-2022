293b8b94232370f862b615c9dea333a1e69bbf95
Tkachev-Igor <Igor9605514546@yandex.ru>
2022-04-08 18:04:57 +0300

    update-image-clusterization

--- image-clusterization/main.cpp
+++ image-clusterization/main.cpp
@@ -1,52 +1,78 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void pass(bool** Arr, int i, int j, int m, int n) {
-    if (i >= 0 && i < m && j >= 0 && j < n)
-    {
-        if (Arr[i][j]) {
-            Arr[i][j] = false;
-            pass(Arr,i+1,j,m,n);
-            pass(Arr,i-1,j,m,n);
-            pass(Arr,i,j+1,m,n);
-            pass(Arr,i,j-1,m,n);
-        }
+struct Node {
+    int x;
+    int y;
+    Node *head;
+    Node *next;
+    Node *tail;
+    Node() {}
+    Node(int a, int b) {
+        x = a;
+        y = b;
+        head = this;
+        next = nullptr;
+        tail = this;
+    }
+};
+
+void join(Node* x, Node* y) {
+    x = x->head;
+    y = y->head;
+    if (x == y) {
+        return;
+    }
+    x->tail->next = y;
+    x->tail = y->tail;
+    while (y != nullptr) {
+        y->head = x;
+        y = y->next;
     }
 }
 
 int main() {
-    int m;
-    cin >> m;
+    int m, n;
+    cin >> m >> n;
 
-    int n;
-    cin >> n;
+    vector<Node*> vec;
 
-    bool** Arr = new bool* [m];
-    for (int i = 0; i < m; i++) {
-        Arr[i] = new bool [n];
-    }
     char c;
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++) {
             cin >> c;
-            if (c == '.') Arr[i][j] = false;
-            else if (c == '#') Arr[i][j] = true;
-        }
-
-    int count = 0;
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++) {
-            if (Arr[i][j]) {
-                Arr[i][j] = false;
-                count++;
-                pass(Arr,i+1,j,m,n);
-                pass(Arr,i-1,j,m,n);
-                pass(Arr,i,j+1,m,n);
-                pass(Arr,i,j-1,m,n);
+            if (c == '#') {
+                Node* node = new Node(i,j);
+                vec.push_back(node);
             }
         }
-    cout << count << endl;
+
+    if (vec.empty()) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    for (int i = 0; i < vec.size() - 1; i++) {
+        for (int j = i + 1; j < vec.size(); j++) {
+            if ((abs(vec[i]->x - vec[j]->x) == 1) && (abs(vec[i]->y - vec[j]->y) == 0) ||
+                (abs(vec[i]->x - vec[j]->x) == 0) && (abs(vec[i]->y - vec[j]->y) == 1)) {
+                    join(vec[i], vec[j]);
+            }
+        }
+    }
+
+    int countUnique = vec.size();
+    for (int i = 0; i < vec.size() - 1; i++)
+        for (int j = i + 1; j < vec.size(); j++) {
+            if (vec[i]->head == vec[j]->head) {
+                countUnique--;
+                break;
+            }
+        }
+
+    cout << countUnique << endl;
 
     return 0;
 }
