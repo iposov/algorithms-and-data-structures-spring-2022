821a5b7865a3ed1b95cc5c5f54b970e8dae2c094
VladisSer <vladisser2ex4@gmail.com>
2022-04-14 02:22:43 +0300

    Do polygon area
    tests are tested by online wolfram with queries like "polygon with vertices (3,1), (7,2), (4,4), (8,6), (1,7), (4,3)"

---
+++ polygon-area/polygon_area.py
@@ -0,0 +1,20 @@
+def area(points):
+    segments_list = list(make_segments(points))
+    area = 0.5 * abs(sum(x0 * y1 - x1 * y0
+                         for ((x0, y0), (x1, y1)) in segments_list))
+    return area
+
+
+def make_segments(points):
+    # делает лист tupl'ов-граней
+    # из исходного списка точек и списка точек со сдвигом 1
+    return zip(points, points[1:] + [points[0]])
+
+
+vertex_count = int(input())
+polygon = []
+for i in range(vertex_count):
+    a = input()
+    polygon.append(tuple(int(el) for el in a.split(' ')))
+
+print(area(polygon))
