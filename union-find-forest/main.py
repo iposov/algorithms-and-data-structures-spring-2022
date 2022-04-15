8f2aac84409f88cdf1abeb20bc6b57b395b71301
ch-anastasiya <nata20001017@gmail.com>
2022-04-16 01:20:25 +0300

    union find forrest

---
+++ union-find-forest/main.py
@@ -0,0 +1,30 @@
+def get(x, p):
+    if p[x] != x:
+        p[x] = get(p[x], p)
+    return p[x]
+
+def UnionFindForrest(x, y, p, r):
+    x = get(x, p)
+    y = get(y, p)
+    if x == y:
+        print("YES")
+        return
+    if r[x] == r[y]:
+        r[x] += 1
+    if r[x] < r[y]:
+        p[x] = y
+    else:
+        p[y] = x
+    print("NO")
+
+
+if __name__ == "__main__":
+    p = []
+    r = []
+    n, k = map(int, input().split())
+    for i in range(n):
+        p.append(i)
+        r.append(1)
+    for i in range(k):
+        x, y = map(int, input().split())
+        UnionFindForrest(x, y, p, r)
