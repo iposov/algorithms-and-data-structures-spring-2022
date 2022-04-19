734f023a23cd53e888a965cc73142c49d96abaca
unknown <telushkinanasta@gmail.com>
2022-04-19 12:13:53 +0300

    add lab4

---
+++ coins-system/src/CoinCounter.java
@@ -0,0 +1,31 @@
+public class CoinCounter {
+
+  private Integer[] coins;
+
+  public CoinCounter(Integer[] coins) {
+    this.coins = coins;
+  }
+
+  public boolean check(int sum) {
+    boolean[] result = new boolean[sum + 1];
+    result[0] = true;
+
+    if (sum == 0) {
+      return true;
+    }
+
+    for (int i = 1; i <= sum; i++) {
+      for (int coin : coins) {
+        if (i - coin >= 0) {
+          if (result[i - coin]) {
+            result[i] = true;
+            break;
+          }
+        }
+      }
+    }
+
+    return result[sum];
+  }
+
+}
